3116efa599c2dea7976f7834edfa1b7727293d4f
thedomingo <domen.hrastnik@gmail.com>
2012-05-22 17:49:46 +0200

    .

---
+++ KTmetoda/Razredi/Alternativa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+
+namespace KTmetoda.Razredi
+{
+    public class Alternativa
+    {
+        public string Naziv { get; set; }
+        public List<Parameter> SeznamParametrov { get; set; }
+    }
+}
